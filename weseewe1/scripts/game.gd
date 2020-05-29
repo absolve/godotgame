bb24d58d18f742aefbe9f232c177713164dfd5c9
absolve <wangding1992@126.com>
2020-05-29 10:36:10 +0800

    更新内容

--- weseewe1/scripts/game.gd
+++ weseewe1/scripts/game.gd
@@ -18,8 +18,11 @@ var sound=true	#声音开关
 
 var blockColor=['#a5aeb3','#22bdd1','#2a6aff','#ffc827','#00b264',
 				'#5f6380','#ff702a','#fdfbc8','#ff4352','#ffa195']
+var lineColor = ["#5f6380"]
 
 
+var group={colorDot="colorDot",scoreDot="scoreDot"}
+	
 	
 func _ready():
 	pass # Replace with function body.
