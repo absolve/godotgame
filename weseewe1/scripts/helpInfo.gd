fe4ee0f466c2c045c85a157e3ce64b565902d5a8
absolve <wangding1992@126.com>
2020-08-03 14:58:00 +0800

    修改weseewe1的对话框

--- weseewe1/scripts/helpInfo.gd
+++ weseewe1/scripts/helpInfo.gd
@@ -31,15 +31,15 @@ func _draw():
 #		draw_line(joints[i].position,dots[i].position,Game.lineColor[0],0.5,true)
 	
 	var v1 = tip1.position
-	draw_line(Vector2(v1.x-100,0),Vector2(v1.x-100,v1.y)
+	draw_line(Vector2(v1.x-50,0),Vector2(v1.x-50,v1.y)
 				,Game.lineColor[0],0.5,true)	
-	draw_line(Vector2(v1.x,0),Vector2(v1.x,v1.y)
+	draw_line(Vector2(v1.x+50,0),Vector2(v1.x+50,v1.y)
 				,Game.lineColor[0],0.5,true)
 	
 	var v2 = tip2.position
-	draw_line(Vector2(v2.x-100,0),Vector2(v2.x-100,v2.y)
+	draw_line(Vector2(v2.x-50,0),Vector2(v2.x-50,v2.y)
 				,Game.lineColor[0],0.5,true)	
-	draw_line(Vector2(v2.x,0),Vector2(v2.x,v2.y)
+	draw_line(Vector2(v2.x+50,0),Vector2(v2.x+50,v2.y)
 				,Game.lineColor[0],0.5,true)
 	
 	
