d081ab7b217a8318ccc37687b30d51f5b4221a73
absolve <wangding1992@126.com>
2020-11-03 19:46:14 +0800

    修改一些代码

--- flappybird1/scripts/pipe.gd
+++ flappybird1/scripts/pipe.gd
@@ -28,7 +28,6 @@ func _physics_process(delta):
 			#当最后一个水管坐标-PIPE_WIDTH时在屏幕外 放到最一根的后面 每个水管的距离是PIPE_WIDTH+OFFSET_X
 			position.x=(PIPE_WIDTH+OFFSET_X)*3-PIPE_WIDTH
 			setRandomYpos()
-		#	position.y=rand_range(OFFSET_Y,game.winHeight-GROUND_HEIGHT-OFFSET_Y)
 
 #设置状态
 func setState(newState):
