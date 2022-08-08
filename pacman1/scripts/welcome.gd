0f1109c7eb9a4634868977e8df449160b1d4d30d
absolve <wangding1992@126.com>
2022-08-08 23:58:29 +0800

    更新功能

--- pacman1/scripts/welcome.gd
+++ pacman1/scripts/welcome.gd
@@ -1,57 +1,60 @@
 extends Node2D
 
 
-var nodes
-var obj=[]
-var pacman=preload("res://scenes/pacman.tscn")
-var ghost=preload("res://scenes/ghost.tscn")
+#var nodes
+#var obj=[]
+#var pacman=preload("res://scenes/pacman.tscn")
+#var ghost=preload("res://scenes/ghost.tscn")
+var pos=Vector3(312,367,0)	#选择1p 2p 时的y坐标
+var index=0
 
+onready var indicator=$indicator
 
 
 func _ready():
-	nodes= load("res://scripts/nodeGroup.gd").new()
 
-	var file = File.new()
-	if file.file_exists("res://maps/test.json"):
-		file.open("res://maps/test.json", File.READ)
-		var currentLevel= parse_json(file.get_as_text())
-		print(currentLevel)
-	
-		nodes.setMapArray(currentLevel['map']['height'],currentLevel['map']['width'])
-		nodes.createNode(currentLevel['data'])
-		
-		file.close()
-	else:
-		print('文件不存在')	
-	
-	add_child(nodes)
-#	obj.append(tempn)
-	
-	var temp=pacman.instance()
-	temp.setStartNode(nodes.data[0][0])
-	add_child(temp)
-	obj.append(temp)
-	
-	var t=ghost.instance()
-#	t.position=tempn.data[0][25].position
-	add_child(t)
-	obj.append(t)
-#	update()	
-
+	pass
 
 
 func _process(delta):
 #	update()
-	for i in obj:
-		
-		i._update(delta)
-	
+#	for i in obj:
+#
+#		i._update(delta)
+#
 	pass
 
 
+func _input(event):
+	if event is InputEventKey:
+		if event.is_pressed():
+			if (event as InputEventKey).scancode==KEY_DOWN:
+				if index<3:
+					index+=1
+					setMode(index)
+				pass
+			elif (event as InputEventKey).scancode==KEY_UP:
+				if index>0:
+					index-=1
+					setMode(index)
+				pass
+			elif (event as InputEventKey).scancode==KEY_ENTER:
+				
+				pass		
+	pass
+
+
+
+func setMode(index):
+	if index==0:
+		indicator.position.y=pos.x
+	elif index==1:
+		indicator.position.y=pos.y
+	elif index==2:
+		indicator.position.y=pos.z
+	pass
+	
+	
 func _draw():
-	for y in nodes.nodeList:
-		for i in y.neighbors.keys():
-			if y.neighbors[i]:
-				draw_line(y.position,y.neighbors[i].position,Color.white,5)
+
 	pass
